f686e5176fa3a8050f3ae53144349a7873649c31
saniya anand <sa2225@afsconnect1.njit.edu>
2017-10-12 18:37:43 -0400

    Index file for uploader

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <form enctype="multipart/form-data" method="POST" action="upload.php">
+      <input type="file" name="fileToUpload" id="fileToUpload" accept="csv" />
+      <input type="submit" name="submitButton" value="Upload File"> 
+    </form>
+  </body>
+</html>
