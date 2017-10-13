7c82dfa417c35b6ecf5878b8d16444cdb0c94c36
saniya anand <sa2225@afsconnect2.njit.edu>
2017-10-12 21:36:00 -0400

    updated upload handler

--- upload.php
+++ upload.php
@@ -1,16 +1,15 @@
 <?php
 
 ini_set('file_uploads', 1);
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+/*ini_set('display_errors', 1);
+error_reporting(E_ALL);*/
 
 $target_dir =  __DIR__ . "/uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
 $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
 $obj = new main();
-$obj->performFileUpload($target_file, $uploadOk, $fileType);
+$obj->performFileUpload($target_file, $fileType);
 
 class main {
     
@@ -18,17 +17,18 @@ class main {
 	} 
 
 	// Main function that handles file uploading
-	function performFileUpload($target_file, $uploadOk, $fileType){
+	function performFileUpload($target_file, $fileType){
 		// Checking if the file doesnt already exist, and that it is of the correct file format
 		if (!$this->isFileAlreadyExisting($target_file) && $this->isCorrectFileFormat($fileType)){
 
-		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			// this command uploads the file to the directory specified, and returns true if successful
+		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) { 
 		        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 		    } else {
 		        echo "Sorry, there was an error uploading your file.";
 		    }
 
-		    //header('Location: http://www.google.com/');
+		    header('Location: https://web.njit.edu/~sa2225/file_upload_project/file_upload_project/upload_success.php?fileName=' . basename($_FILES["fileToUpload"]["name"]));
 
 		} 
 		// If file aready exists or is of incorrect format
