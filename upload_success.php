bf2d752776d8f8ac98859b8e0deb2a7347130e66
saniya anand <sa2225@afsconnect2.njit.edu>
2017-10-14 16:08:13 -0400

    updated upload success file

--- upload_success.php
+++ upload_success.php
@@ -1,4 +1,11 @@
 <?php
-$target_dir =  __DIR__ . "/uploads/" . $_GET['fileName'];
-echo $target_dir;
+
+$target_file =  __DIR__ . "/uploads/" . $_GET['fileName'];
+
+$file = fopen($target_file,"r");
+
+$file_contents_array = fgetcsv($file));
+
+print_r($file_contents_array);
+
 ?>
