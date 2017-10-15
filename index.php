0408aea18b27defde606ad4c76ae33cb522940c5
saniya anand <sa2225@afsconnect2.njit.edu>
2017-10-14 22:05:48 -0400

    updated code

--- index.php
+++ index.php
@@ -87,22 +87,56 @@ class uploadform extends page
  		$form .= '<h1>View Your CSV files!</h1><br><br>';
  		$form .= '<h3>Please upload your CSV below to view it:</h3><br><br>';
  		$form .= '<form enctype="multipart/form-data" method="POST" action="index.php?page=uploadform">';
- 		$form .= '<input type="file" name="fileToUpload" id="fileToUpload"><br>';
-        $form .= '<input type="submit" value="Upload Image" name="Upload & View">';
+ 		$form .= '<input type="file" name="fileToUpload" id="fileToUpload"><br><br>';
+        $form .= '<input type="submit" value="Upload & View" name="submit">';
         $form .= '</form> ';
-        $this->html .= '<h1>Upload Form</h1>';
         $this->html .= $form;
     }
 
     public function post() {
-        echo 'test';
-        print_r($_FILES);
+        $target_dir =  __DIR__ . "/uploads/";
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+		performFileUpload($target_file, $fileType);
     }
+
+    // Main function that handles file uploading
+	private function performFileUpload($target_file, $fileType){
+		// Checking if the file doesnt already exist, and that it is of the correct file format
+		if (!$this->isFileAlreadyExisting($target_file)){
+			// this command uploads the file to the directory specified, and returns true if successful
+		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) { 
+		        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		        header('Location: https://web.njit.edu/~sa2225/file_upload_project/file_upload_project/index.php?page=uploadsuccess');
+		    } else {
+		        echo "Sorry, there was an error uploading your file.";
+		    }
+		} 
+		// If file aready exists or is of incorrect format
+		else {
+			echo "Sorry, there was an error uploading your file.";
+		}
+
+	}
+
+	//Check if the file exists
+	private function isFileAlreadyExisting($target_file){
+		
+		if (file_exists($target_file)) {
+		    echo "File already exists.";
+		    return true;
+		} else {
+			return false;
+		}
+	}
 }
 
 
 
-class htmlTable extends page {}
+class uploadsuccess extends page {
+
+	echo 'TESTING';
+}
 
 
 ?>
