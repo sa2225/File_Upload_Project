441b4ab1a825dfdde197c7dc5f203420e06e8a26
saniya anand <sa2225@afsconnect2.njit.edu>
2017-10-14 21:19:51 -0400

    updated index page

--- index.php
+++ index.php
@@ -8,19 +8,22 @@
 			table tr:nth-child(even) {background-color: #F7B801;font-size: 12px;}
 			table tr:nth-child(odd) {background-color: #E8EBE4;font-size: 12px;}
 			table {width: 100%;font-size: 13px;}
-			table, th, td {border: 1px solid black;border-collapse: collapse;} 
+			table, th, td {border: 1px solid black;border-collapse: collapse;}
+			.divmidfloater {margin: 0 auto;}
 		</style>
 	</head>
-	<body>    
-	<h1>View Your CSV files!</h1>
-	<br>
-	<br>	
-	<h3>Please upload your CSV below to view it:</h3>
-	<br>
-	<br>	
-	<form enctype="multipart/form-data" method="POST" action="upload.php">
-      <input type="file" name="fileToUpload" id="fileToUpload" accept=".csv" />
-      <input type="submit" name="submitButton" value="Upload File"> 
-    </form>
+	<body>
+		<div class="divmidfloater">    
+			<h1>View Your CSV files!</h1>
+			<br>
+			<br>	
+			<h3>Please upload your CSV below to view it:</h3>
+			<br>
+			<br>	
+			<form enctype="multipart/form-data" method="POST" action="upload.php">
+		      <input type="file" name="fileToUpload" id="fileToUpload" accept=".csv" />
+		      <input type="submit" name="submitButton" value="Upload File"> 
+		    </form>
+		</div>
   </body>
 </html>
