75e8bae305751f4baaf6e65d2d54305b04fb378b
saniya anand <sa2225@afsconnect2.njit.edu>
2017-10-14 21:10:54 -0400

    updated upload success handler

--- upload_success.php
+++ upload_success.php
@@ -6,6 +6,7 @@
 		table tr:first-child{font-weight: bold;background-color: #7678ED !important;font-size: 12px;}
 		table tr:nth-child(even) {background-color: #F7B801;font-size: 12px;}
 		table {width: 100%;font-size: 12px;}
+		table, th, td {border: 1px solid black;border-collapse: collapse;} 
 	</style>
 </head>
 <body>
