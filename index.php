6b385db82246b9c58da3331401a78d0ffbf8ca11
saniya anand <sa2225@afsconnect1.njit.edu>
2017-10-16 23:02:36 -0400

    Updated code for quality

--- index.php
+++ index.php
@@ -58,25 +58,25 @@ abstract class page {
         print_r($this->html);
     }
 
-	public function get() {
+    public function get() {
         
     }
 
     public function post() {
-        print_r($_POST);
+        //print_r($_POST);
     }
 }
 
 class uploadform extends page
 {
-	// Generate and display the upload form
+    // Generate and display the upload form
     public function get()
     {
-		$form = '<div class="divmidfloater">';
- 		$form .= '<h1>View Your CSV files!</h1><br><br>';
- 		$form .= '<h3>Please upload your CSV below to view it:</h3><br><br>';
- 		$form .= '<form enctype="multipart/form-data" method="POST" action="index.php?page=uploadform">';
- 		$form .= '<input type="file" name="fileToUpload" id="fileToUpload" accept=".csv"><br><br>';
+        $form = '<div class="divmidfloater">';
+        $form .= '<h1>View Your CSV files!</h1><br><br>';
+        $form .= '<h3>Please upload your CSV below to view it:</h3><br><br>';
+        $form .= '<form enctype="multipart/form-data" method="POST" action="index.php?page=uploadform">';
+        $form .= '<input type="file" name="fileToUpload" id="fileToUpload" accept=".csv"><br><br>';
         $form .= '<input type="submit" value="Upload & View" name="submit">';
         $form .= '</form> ';
         $this->html .= $form;
@@ -85,51 +85,51 @@ class uploadform extends page
     // Handle the submission of the form and uploading of the file
     public function post() {
 
-    	// Defining the target directory where the file will be stored
+        // Defining the target directory where the file will be stored
         $target_dir =  __DIR__ . "/uploads/";
 
         // Defining the full path - target directory + filename with extension
-		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-		$this->performFileUpload($target_file);
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $this->performFileUpload($target_file);
     }
 
     // Main function that handles file uploading
-	private function performFileUpload($target_file){
-		// Checking if the file doesnt already exist, and that it is of the correct file format
-		if (!$this->isFileAlreadyExisting($target_file)){
-			// this command uploads the file to the directory specified, and returns true if successful
-		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) { 
-		        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-		        header('Location: https://web.njit.edu/~sa2225/file_upload_project/file_upload_project/index.php?page=uploadsuccess&fileName=' . urlencode(basename($_FILES["fileToUpload"]["name"])));
-		    } else {
-		        echo "Sorry, there was an error uploading your file.";
-				echo '<br><br><input type="button" value="Upload another file" onclick="history.back()">';
-		    }
-		} 
-		// If file aready exists or is of incorrect format
-		else {
-			echo "Sorry, there was an error uploading your file.";
-		    echo '<br><br><input type="button" value="Upload another file" onclick="history.back()">';
-		}
+    private function performFileUpload($target_file){
+        // Checking if the file doesnt already exist, and that it is of the correct file format
+        if (!$this->isFileAlreadyExisting($target_file)){
+            // this command uploads the file to the directory specified, and returns true if successful
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) { 
+                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+                header('Location: https://web.njit.edu/~sa2225/file_upload_project/file_upload_project/index.php?page=uploadsuccess&fileName=' . urlencode(basename($_FILES["fileToUpload"]["name"])));
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+                echo '<br><br><input type="button" value="Upload another file" onclick="history.back()">';
+            }
+        } 
+        // If file aready exists or is of incorrect format
+        else {
+            echo "Sorry, there was an error uploading your file.";
+            echo '<br><br><input type="button" value="Upload another file" onclick="history.back()">';
+        }
 
-	}
+    }
 
-	//Check if the file exists
-	private function isFileAlreadyExisting($target_file){
-		// Checking the existing file path 
-		if (file_exists($target_file)) {
-		    echo "File already exists.";
-		    return true;
-		} else {
-			return false;
-		}
-	}
+    //Check if the file exists
+    private function isFileAlreadyExisting($target_file){
+        // Checking the existing file path 
+        if (file_exists($target_file)) {
+            echo "File already exists.";
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 class uploadsuccess extends page {
     
     // Show initial messages for upload success and call function to generate table
-    public function __construct(){
+    public function get(){
         $this->html .= '<html>';
         $this->html .= '<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">';
         $this->html .= '<link rel="stylesheet" href="styles.css">';
@@ -141,42 +141,36 @@ class uploadsuccess extends page {
         $this->html .= '</h3><br><br>';
         $this->html .= '<input type="button" value="Upload another file" onclick="history.back()"><br><br>';
         $this->html .= '</div>';
-		$target_file =  __DIR__ . "/uploads/" . $_REQUEST['fileName'];
+        $target_file =  __DIR__ . "/uploads/" . $_REQUEST['fileName'];
         $this->displayFileContents($target_file);
     }
 
-	// Main function that handles displaying the file by generating a table
-	private function displayFileContents($target_file){
-		
-		//Opened file stream
-		$file = fopen($target_file,"r");
-		$this->html .= '<table>';
-		$firstRow = true;
-		// Looping on the file to check if data lines exist - then print each value per line in a td
-		while (($line = fgetcsv($file)) !== false) {
-	        $this->html .= '<tr>';
-	    	if($firstRow){
-				foreach ($line as $cell) {
-	                $this->html .=  '<th>' . htmlspecialchars($cell) . '</th>';
-		        }
-		        $firstRow = false;
-	    	} else {   
-		        foreach ($line as $cell) {
-	                $this->html .=  '<td>' . htmlspecialchars($cell) . '</td>';
-		        }
-	    	}
-	        $this->html .= '</tr>';
-		}
-		// Closed file stream
-		fclose($file);
+    // Main function that handles displaying the file by generating a table
+    private function displayFileContents($target_file){
+        
+        //Opened file stream
+        $file = fopen($target_file,"r");
+        $this->html .= '<table>';
+        $firstRow = true;
+        // Looping on the file to check if data lines exist - then print each value per line in a td
+        while (($line = fgetcsv($file)) !== false) {
+            $this->html .= '<tr>';
+            if($firstRow){
+                foreach ($line as $cell) {
+                    $this->html .=  '<th>' . htmlspecialchars($cell) . '</th>';
+                }
+                $firstRow = false;
+            } else {   
+                foreach ($line as $cell) {
+                    $this->html .=  '<td>' . htmlspecialchars($cell) . '</td>';
+                }
+            }
+            $this->html .= '</tr>';
+        }
+        // Closed file stream
+        fclose($file);
 
-		$this->html .= '</table>';
-	}
-
-	// Closing the HTML file
-	public function __destruct(){
-        $this->html .= '</body></html>';
-        print_r($this->html);
+        $this->html .= '</table>';
     }
 }
 ?>
